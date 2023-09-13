77b530cbe2a32028a88f196d315757cbb97427f2
rajesh <rajeshupadhayaya68@gmail.com>
2023-09-13 17:18:30 -0600

    Bootstrap and about page included.

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+# Static dirs for providing a list of additional directories for Django to look  in.
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# To collect all the static file at a location defined by static_root for production.
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# it is a file storage engine used when collecting static files with the collectstatic command.
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from pages.views import HomePageView
+from pages.views import HomePageView, AboutPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
