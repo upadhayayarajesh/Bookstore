6b08e0f173b53d2a3faff01b0d32e17b8d597f4e
rajesh <rajeshUpadhayaya68@gmail.com>
2023-09-16 13:50:58 -0600

    Review model added.

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from books.models import Book
+from books.models import Book, Review
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInLine]
     list_display = (
         "title",
         "author",
--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -15,3 +16,18 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[str(self.id)])
+
+
+class Review(models.Model):
+    # for many to one field we must also specify an on_delete option.
+    # we also explicitly set the related_name to make it easier to follow the foreign key relation
+    # backwards in the future projects.
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
