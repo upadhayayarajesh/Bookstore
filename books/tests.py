617b15de74202c04e487f20e708758a89311ac1d
rajesh <rajeshUpadhayaya68@gmail.com>
2023-09-16 15:38:34 -0600

    Tests setup for the loging users.

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase, override_settings
 from django.urls import reverse
 
@@ -21,6 +22,8 @@ class BookTests(TestCase):
         cls.user = get_user_model().objects.create_user(
             username="reviewuser", email="reviewuser@email.com", password="testpass123"
         )
+        cls.special_permission = Permission.objects.get(codename="special_status")
+
         cls.book = Book.objects.create(
             title="Harry Potter",
             author="JK Rowling",
@@ -38,15 +41,18 @@ class BookTests(TestCase):
         self.assertEqual(f"{self.book.author}", "JK Rowling")
         self.assertEqual(f"{self.book.price}", "25.00")
 
-    def test_book_list_view(self):
+    def test_book_listing_view_for_logged_in_user(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123")
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, "books/book_list.html")
 
-    def test_book_detail_view(self):
+    def test_book_listing_view_for_logged_out_user(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
-        no_response = self.client.get("/books/12345/")
+        no_response = self.client.get("/books/12345")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
